f4f974dd5a6a33efed6fc3ceaaf94eb83b697209
Dani12311 <dani.casteleijn@gmail.com>
2026-05-12 12:25:35 +1000

    test

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/card")
+def card():
+    return "THIS IS A TEST"
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
